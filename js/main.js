651dcdb505badd11b603e0fd6c7bdc07e58a2ac0
Grand Marcell <grand1310marcell@gmail.com>
2022-04-25 20:46:59 +0700

    Done

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-const selectedRating = Array.from(document.querySelectorAll(".star-value"))
+var ratingContainer = document.querySelector(".rating-option")
+var ratings = ratingContainer.getElementsByClassName("star-value")
 
-selectedRating.forEach((e) => {
-    e.addEventListener("click", () => {
-        selectedRating.forEach((e) => {
-            e.classList.remove("selected")
-        })
-        e.classList.add("selected")
+for(var i = 0; i < ratings.length; i++){
+    ratings[i].addEventListener("click", function(){
+        var current = document.getElementsByClassName("selected")
+        if (current.length > 0){
+            current[0].className = current[0].className.replace(" selected", "")
+        }
+        this.className += " selected"
     })
+}
+
+const submitbtn = document.querySelector(".submit")
+submitbtn.addEventListener("click", () => {
+    for(var i = 0; i < ratings.length; i++){
+        if(ratings[i].classList.contains("selected")){
+            document.getElementById("result").textContent = i + 1
+        }
+    }
+    document.querySelector(".rating").style.display = "none"
+    document.querySelector(".thankyou-state").style.display = "flex"
 })
